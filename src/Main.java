1d3be226a28476cee0518ba2e8803afc1cae0100
Piotr Grohs <piotrgrohs@gmail.com>
2020-04-04 17:14:41 +0200

    add connector to mysql db

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,16 @@
-import org.hibernate.HibernateException;
-import org.hibernate.Metamodel;
+import model.Osoba;
+import org.hibernate.*;
 import org.hibernate.query.Query;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
 import javax.persistence.metamodel.EntityType;
 
+import java.util.GregorianCalendar;
 import java.util.Map;
 
 public class Main {
     private static final SessionFactory ourSessionFactory;
-
+    private static final SessionFactory SESSION_FACTORY = HiberUtil.getSessionFactory(HiberUtil.Mapping.XML);
     static {
         try {
             Configuration configuration = new Configuration();
@@ -26,7 +25,20 @@ public class Main {
     public static Session getSession() throws HibernateException {
         return ourSessionFactory.openSession();
     }
-
+    private static void createOsoby(){
+        try (Session session = SESSION_FACTORY.openSession()) {
+            Transaction tx = session.beginTransaction();
+            Osoba o1 = new Osoba("Jan", "Kowalski", "52062437366");
+            session.save(o1);
+            Osoba o2 = new Osoba("Tomasz", "Barański", "61091696417");
+            session.save(o2);
+            Osoba o3 = new Osoba("Marek", "Pyka", "63100375524");
+            session.save(o1);
+            Osoba o4 = new Osoba("Bartek", "Que", "71102147175");
+            session.save(o2);
+            tx.commit();
+        }
+    }
     public static void main(final String[] args) throws Exception {
         final Session session = getSession();
         try {
