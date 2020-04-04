4b1fbc92874a7237fb17ee5bb86d1340b8d5c568
Piotr Grohs <piotrgrohs@gmail.com>
2020-04-04 16:34:24 +0200

    hiberutil

---
+++ src/HiberUtil.java
@@ -0,0 +1,91 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import model.*;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import java.io.File;
+
+/**
+ * 
+ * @author zti
+ */
+
+public final class HiberUtil {
+    /**
+     * choose type of mapping definition XML or annotations
+     */
+    public enum Mapping {
+       /**
+        * use xml defined mappings for configuration
+        */
+       XML, 
+       /**
+        * use annotation defined mappings for confuguration
+        */
+       ANN;
+    } 
+    /**
+     * 
+     * @param mapping
+     * @return SessionFactory 
+     */
+    public static SessionFactory getSessionFactory(Mapping mapping) {
+     
+        switch(mapping){
+            case XML:
+                return(getXMLSessionFactory());
+            case ANN:
+                return(getANNSessionFactory());
+            default:
+               return(getXMLSessionFactory());
+        }  
+    }
+  
+    public static SessionFactory getXMLSessionFactory() {
+        try {
+            File mappingDir = new File("src\\mapowanie");
+            Configuration config = new Configuration().configure();
+
+//            config.setProperty("hibernate.show_sql", "false");
+            config.addDirectory(mappingDir);
+            SessionFactory sf = config.buildSessionFactory();
+            return (sf);
+        }
+        catch (Throwable ex) {
+          
+            System.err.println("Initial SessionFactory creation failed." + ex);
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+
+ public static SessionFactory getANNSessionFactory() {
+        try {
+            Configuration config = new Configuration().configure();
+//            config.setProperty("hibernate.show_sql", "false");
+
+            Configuration configuration = config.addAnnotatedClass(Adres.class)
+                    .addAnnotatedClass(Cennik.class)
+                    .addAnnotatedClass(Lekarz.class)
+                    .addAnnotatedClass(Osoba.class)
+                    .addAnnotatedClass(Pacjent.class)
+                    .addAnnotatedClass(Usluga.class)
+                    .addAnnotatedClass(Wizyta.class)
+                    .addAnnotatedClass(Zabieg.class);
+            SessionFactory sf = config.buildSessionFactory();
+            return (sf);
+        }
+        catch (Throwable ex) {
+          
+            System.err.println("Initial SessionFactory creation failed." + ex);
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+
+ 
+ 
+}
